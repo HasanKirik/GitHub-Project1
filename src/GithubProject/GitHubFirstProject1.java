0dea11cbb54c8b7660737fe6909eb69860b541b9
User <hasankrk78@hotmail.com>
2022-12-26 18:54:42 -0500

    firstProjectt

---
+++ src/GithubProject/GitHubFirstProject1.java
@@ -0,0 +1,9 @@
+package GithubProject;
+
+import javax.sound.midi.Soundbank;
+
+public class GitHubFirstProject1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Hasan ");
+    }
+}

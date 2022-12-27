70a4884d0834766bff3f46af0eba09cd367c5971
User <hasankrk78@hotmail.com>
2022-12-26 19:35:21 -0500

    ilk degisiklik

--- src/GithubProject/GitHubFirstProject1.java
+++ src/GithubProject/GitHubFirstProject1.java
@@ -5,5 +5,6 @@ import javax.sound.midi.Soundbank;
 public class GitHubFirstProject1 {
     public static void main(String[] args) {
         System.out.println("Hello Hasan ");
+        System.out.println("I  mess my family");
     }
 }
